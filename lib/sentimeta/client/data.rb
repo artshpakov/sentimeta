b0da24302a45180530d53bed50dd347ab6da4fb2
Eugene Korneev <ekorneeff@gmail.com>
2015-01-15 15:46:29 +0300

    Search

--- lib/sentimeta/client/data.rb
+++ lib/sentimeta/client/data.rb
@@ -11,4 +11,26 @@ module Sentimeta::Client::Data
   def attributes options={}
     get(:attributes, options)['values']
   end
+
+  ##
+  # Usage:
+  #   search text: 'pitt' [, where: 'actors' ]
+  def search options={}
+    if options.has_key? :where
+      search_local(options[:where], options[:text])[options[:where]]
+    else
+      search_global options[:text]
+    end
+  end
+
+  private
+
+  def search_global(text)
+    get :search, { text: text }
+  end
+
+  def search_local(where, text)
+    get :search, { where: where, text: text }
+  end
+
 end
