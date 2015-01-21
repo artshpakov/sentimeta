28dbeeafbb747d985f570af6fa1f609434a05bb6
Artem Shpakov <artshpakov@yandex.ru>
2015-01-21 18:40:16 +0300

    New auth endpoints

--- lib/sentimeta/client/auth.rb
+++ lib/sentimeta/client/auth.rb
@@ -2,33 +2,33 @@ module Sentimeta::Client::Auth
   extend self
   extend Sentimeta::RestClient
 
-  self.namespace = :auth
+  self.namespace = 'crowd/auth'
 
   def user token
-    get :users, id: token
+    get 'users/token', id: token
   end
 
   def signup attrs
-    post :users, user: attrs
+    post :signup, attrs
   end
 
   def signin attrs
-    post :sessions, user: attrs
+    post :signin, attrs
   end
 
-  def reset_password_token email
-    put 'passwords/create_token', email: email
+  def reset_password_token login
+    put 'users/create_reset_token', login: login
   end
 
   def reset_password params
-    put 'passwords/change', params
+    put 'users/reset_password', params
   end
 
   def oauth attrs
-    post :oauth, identity: attrs
+    post :oauth, attrs
   end
 
   def signout token
-    delete :sessions, id: token
+    delete 'users/token', id: token
   end
 end
