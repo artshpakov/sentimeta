5b97264384dccc09b8a4d390c398828063f33edc
Artem Shpakov <artshpakov@yandex.ru>
2015-01-29 13:54:01 +0300

    Prices quickfix

--- lib/sentimeta/client/prices.rb
+++ lib/sentimeta/client/prices.rb
@@ -2,6 +2,8 @@ module Sentimeta::Client::Prices
   extend self
   extend Sentimeta::RestClient
 
+  def sphere() :hotels end
+
   def load options={}
     options[:id] = options.delete(:provider)
     get :prices, options
